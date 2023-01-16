657b2daf5f807aacfa0bb2c80ce1494c1b96023a
bynikk <bynik2001@gmail.com>
2023-01-16 05:23:48 +0300

    add data access extension

---
+++ src/BetService.DataAccess/DataAccessServicesExtensions.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace BetService.DataAccess
+{
+    public static class DataAccessServicesExtensions
+    {
+        /// <summary>Register the mongo database context.</summary>
+        /// <param name="services">The services collection.</param>
+        /// <returns>
+        ///   The services collection.
+        /// </returns>
+        public static IServiceCollection AddMongoDbContext(this IServiceCollection services)
+        {
+            services.AddScoped<BetDbContext>()
+                    .AddScoped(serviceProvider =>
+                serviceProvider.GetRequiredService<BetDbContext>().OutcomeGroupCollection)
+                    .AddScoped(serviceProvider =>
+                serviceProvider.GetRequiredService<BetDbContext>().BetCollection)
+                    .AddScoped(serviceProvider =>
+                serviceProvider.GetRequiredService<BetDbContext>().OutcomeGroupCollection)
+                    .AddScoped(serviceProvider =>
+                serviceProvider.GetRequiredService<BetDbContext>().TeamCollection);
+
+            services.AddSingleton(serviceProvider =>
+            {
+                var options = serviceProvider.GetRequiredService<IOptions<BetDbConfig>>();
+                var client = new MongoClient(options.Value.ConnectionString);
+
+                return client.GetDatabase(options.Value.DatabaseName);
+            });
+
+            return services;
+        }
+
+        /// <summary>Register the mongo repositories in service collection.</summary>
+        /// <param name="services">The services collection.</param>
+        /// <returns>
+        ///   The services collection.
+        /// </returns>
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        /// <summary>Register the mongo providers in service collection.</summary>
+        /// <param name="services">The services collection.</param>
+        /// <returns>
+        ///   The services collection.
+        /// </returns>
+        public static IServiceCollection AddProviders(this IServiceCollection services)
+        {
+            return services;
+        }
+
+
+    }
+}
--- src/BetService.Grpc/Program.cs
+++ src/BetService.Grpc/Program.cs
@@ -1,18 +1,21 @@
 ﻿using BetService.BusinessLogic.Models.Competitions;
+using BetService.DataAccess;
 using BetService.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+var configuration = builder.Configuration;
 
-// Add services to the container.
-builder.Services.AddGrpc();
+builder.Services.Configure<BetDbConfig>(
+    builder.Configuration.GetSection("BetDbConfig"));
+
+builder.Services
+    .AddMongoDbContext()
+    .AddGrpc();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapGet("/", () => "It's works!");
 
 app.Run();
