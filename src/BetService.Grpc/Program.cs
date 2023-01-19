0a90fb12e0683b1adfeed2e310b397771a2debec
bynikk <bynik2001@gmail.com>
2023-01-19 12:56:53 +0300

    fix protocol problem

--- src/BetService.Grpc/Program.cs
+++ src/BetService.Grpc/Program.cs
@@ -17,10 +17,6 @@ builder.Services
 var app = builder.Build();
 
 app.MapGrpcService<BetService.Grpc.Services.BetService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
-
-namespace BetService.Grpc
-{
-    public partial class Program { }
-}
--- src/BetService.Grpc/Services/BetService.cs
+++ src/BetService.Grpc/Services/BetService.cs
@@ -1,5 +1,6 @@
 ﻿using BetService.BusinessLogic.Contracts.Providers;
 using BetService.BusinessLogic.Models.Competitions;
+using BetService.Grpc;
 using Grpc.Core;
 
 namespace BetService.Grpc.Services
@@ -7,17 +8,21 @@ namespace BetService.Grpc.Services
     public class BetService : Grpc.BetService.BetServiceBase
     {
         private readonly ICompetitionProvider<CompetitionCS> _competitionProvider;
+        private readonly ILogger<BetService> _logger;
 
-        public BetService(ICompetitionProvider<CompetitionCS> competitionProvider)
+        public BetService(ILogger<BetService> logger, ICompetitionProvider<CompetitionCS> competitionProvider)
         {
+            _logger = logger;
             _competitionProvider = competitionProvider;
         }
 
-        public override async Task<GetCompetitionsResponse> GetCompetitions(GetCompetitionsRequest request, ServerCallContext context)
+        public override async Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
         {
             var a = await _competitionProvider.GetCompetitionsByStatusType(BusinessLogic.Enums.CompetitionStatusType.Live, 4, 4, CancellationToken.None);
-
-            return new GetCompetitionsResponse();
+            return new HelloReply
+            {
+                Message = "Hello " + request.Name
+            };
         }
     }
 }
