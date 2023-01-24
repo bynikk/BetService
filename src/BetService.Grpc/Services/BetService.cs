44e968fcefd92edaad05eb1cea70dc1e1c05c5d5
nikita.prachkovski <nikita.prahkovski@effective-soft.com>
2023-01-24 17:21:58 +0300

    refactory grpc service implementation

--- src/BetService.Grpc/Services/BetService.cs
+++ src/BetService.Grpc/Services/BetService.cs
@@ -12,19 +12,17 @@ namespace BetService.Grpc.Services
     public class BetService : Grpc.BetService.BetServiceBase
     {
         private readonly IMapper _mapper;
-        private readonly ICoefficientRepository _coefficientRepository;
         private readonly ILogger<BetService> _logger;
         private readonly ICompetitionService<BussinessModels.Competitions.CompetitionDota2> _competitionDota2Service;
 
         public BetService(
             ILogger<BetService> logger,
             ICompetitionService<BussinessModels.Competitions.CompetitionDota2> competitionDota2Service,
-            IMapper mapper, ICoefficientRepository coefficientRepository)
+            IMapper mapper)
         {
             _logger = logger;
             _competitionDota2Service = competitionDota2Service;
             _mapper = mapper;
-            _coefficientRepository = coefficientRepository;
         }
 
         public override async Task<CreateCompetitionDota2Response> CreateCompetitionDota2(CreateCompetitionDota2Request request, ServerCallContext context)
@@ -50,11 +48,7 @@ namespace BetService.Grpc.Services
             var grpcCompetitions = _mapper.Map<IEnumerable<CompetitionDota2>>(competitions);
 
             response.CompetitionsDota2.AddRange(grpcCompetitions);
-            await _coefficientRepository.DepositAmountById(
-                new Guid("b350c9fd-3632-4b0a-b5cb-66e41d530f55"),
-                new Guid("3c203bd7-2d7e-4937-a82a-e451cedf2ba8"),
-                10,
-                token);
+  
             return response;
         }
 
