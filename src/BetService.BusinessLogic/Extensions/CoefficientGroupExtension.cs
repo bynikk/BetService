c90a27356aac6a6a9bb78226df40904adb0d0153
nikita.prachkovski <nikita.prahkovski@effective-soft.com>
2023-01-25 13:15:35 +0300

    add recalculate rate implementation

---
+++ src/BetService.BusinessLogic/Extensions/CoefficientGroupExtension.cs
@@ -0,0 +1,30 @@
+﻿using BetService.BusinessLogic.Models;
+using BetService.BusinessLogic.Models.Competitions;
+
+namespace BetService.BusinessLogic.Extensions
+{
+    public static class CoefficientGroupExtension
+    {
+        private static readonly double _expectedTotalProbability = 100;
+        private static readonly double _systemFee = 5;
+
+        public static CoefficientGroup RecalculateRate(this CoefficientGroup coefficientGroup)
+        {
+            var totalProbability = coefficientGroup.Coefficients.Sum(x => x.Probability);
+            if (totalProbability != _expectedTotalProbability)
+            {
+                throw new ArgumentException(
+                    $"Total probability do not reached. Current probability of coefficientGroup with id {coefficientGroup.Id} is {totalProbability}");
+            }
+
+            var actualProbability = _expectedTotalProbability - _systemFee;
+
+            for (var i = 0; i < coefficientGroup.Coefficients.Count; i++)
+            {
+                coefficientGroup.Coefficients[i].Rate = actualProbability / coefficientGroup.Coefficients[i].Probability;
+            }
+
+            return coefficientGroup;
+        }
+    }
+}
---
+++ src/BetService.BusinessLogic/Extensions/CompetitionBaseExtension.cs
@@ -0,0 +1,27 @@
+﻿using BetService.BusinessLogic.Models.Competitions;
+
+namespace BetService.BusinessLogic.Extensions
+{
+    public static class CompetitionBaseExtension
+    {
+        /// <summary>
+        /// Recalculate rate of coefficient and posability accordint with current beted amount
+        /// or posability if required beted amount has been reached.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="competitionsBase"></param>
+        /// <returns>Competition model inherited from <seealso cref="CompetitionBase"/> </returns>
+        public static T RecalculateCoefficientsAndPosability<T>(this T competitionsBase) where T : CompetitionBase
+        {
+            var coefficientsGroups = competitionsBase.CoefficientGroups;
+            var length = coefficientsGroups.Count;
+
+            for (var i = 0; i < length; i++)
+            {
+                coefficientsGroups[i].RecalculateRate();
+            }
+
+            return competitionsBase;
+        }
+    }
+}
